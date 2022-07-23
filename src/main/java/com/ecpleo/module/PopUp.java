a1b768ffabcbee8984efa7f1d2eea63f6bc6d835
VinuthaSamsundar <vinuthasundaran@gmail.com>
2022-07-23 20:10:39 +0100

    Cookie object  added.

--- src/main/java/com/ecpleo/module/PopUp.java
+++ src/main/java/com/ecpleo/module/PopUp.java
@@ -8,9 +8,10 @@ public class PopUp {
     Helper helper = Helper.getInstance();
 
     By getMoreCloseButton = By.xpath("//button[@role='button' and contains(@id,'yie-close-button')]");
-
+    By cookieButton = By.xpath("//*[@id=\"__next\"]/div[3]/section/div[2]/button[2]");
     public void closeGetMore(){
         helper.waitForBrowserToLoadCompletely();
+        helper.clickObject(cookieButton);
         helper.clickObject(getMoreCloseButton);
     }
 
